125dabdcdd7e198bdf9c1a010cd7eee80294f4e4
Karen Garcia <karen.garcia@nexscreen.com>
2019-08-29 14:29:10 -0600

    got it to work with functional component rather than a class with componentDidMount

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,57 +1,94 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 
-// function App() {
-//   return (
-//     <div className="App">
-      
-//     </div>
-//   );
-// }
+const App = () => {
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state= {
-      users: []
+  const [users, setUsers] = useState([]);
+  const [hasError, setErrors] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('http://localhost:3000/users');
+      res
+      .json()
+      .then(res => setUsers(res))
+      .catch(err => setErrors(err));
     }
-  }
 
-  componentDidMount(){
-    fetch('http://localhost:3000/users')
-      .then(response => response.json())
-      .then(res => {
-        if (res && res.data){
-          this.setState({ users: [...this.state.users, ...res.data]})
-        }
-      });
-  }
+    fetchData();
+  });
 
-  renderUsers(){
-    if (this.state.users.length <= 0){
+  const renderUsers = () =>{
+    if (users.length <= 0){
       return <div>Loading..</div>
     }else{
       return (
-        this.state.users.map((val, key) => {
+        users.map((val, key) => {
           return (
           <div key={key}>
             {val.name} | {val.age}
-          </div>);
+          </div>
+          );
         })
       )
     }
-  }
+  };
 
-  render(){
-    return(
-
+  return(
     <div className="App">
-      { this.renderUsers() }
+      {renderUsers()}
+      {JSON.stringify(users)}
+      <span>Has error: {JSON.stringify(hasError)}</span>
     </div>
 
     );
-  }
-}
+  
+};
+
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+//     this.state= {
+//       users: []
+//     }
+//   }
+
+//   componentDidMount(){
+//     fetch('http://localhost:3000/users')
+//       .then(response => response.json())
+//       .then(res => {
+//         if (res && res.data){
+//           this.setState({ users: [...this.state.users, ...res.data]})
+//         }
+//       });
+//   }
+
+//   renderUsers(){
+//     if (this.state.users.length <= 0){
+//       return <div>Loading..</div>
+//     }else{
+//       return (
+//         this.state.users.map((val, key) => {
+//           return (
+//           <div key={key}>
+//             {val.name} | {val.age}
+//           </div>);
+//         })
+//       )
+//     }
+//   }
+
+//   render(){
+//     return(
+
+//     <div className="App">
+//       { this.renderUsers() }
+//     </div>
+
+//     );
+//   }
+// }
 
 export default App;
